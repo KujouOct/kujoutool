b224256b1a3a0e03391e8af20ae5c37c9fec9509
root <root@vultr.guest>
2021-03-23 14:05:33 +0000

    0323 bug fix

--- ss/ssr-server/ssrConf.sh
+++ ss/ssr-server/ssrConf.sh
@@ -6,7 +6,7 @@ echo "2404:6800:4008:c06::be scholar.google.com
 2401:3800:4001:10::101f scholar.google.com.cn"
 echo "to /etc/hosts"
 uname -a 
-sleep(0.5)
+sleep 0.5
 vim /etc/shadowsocks-r/config.json
 service shadowsocks-r stop
 service shadowsocks-r start
